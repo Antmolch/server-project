2608f6489d3be0f62d6b7f8ea7e9377ca0597aec
Antmolch <darkraven653@gmail.com>
2023-05-05 17:05:30 +0800

    update merge back and front

--- Front/src/components/Constructor/Mail.js
+++ Front/src/components/Constructor/Mail.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import plusIcon from '../img/plus-svg.svg'
 import exitIcon from '../img/exit.svg'
 import Message from './Message'
 import arrowIcon from '../img/arrow-bottom.svg'
+
+import FileList from './list-components/FileList';
+import Modal from '../Modal/Modal';
+
+import '../css/errors.css'
+import { Guid } from 'js-guid';
 
 
 export function Mail(props){
@@ -12,10 +18,32 @@ export function Mail(props){
     const command_index = bot.commands.findIndex(x => x.id === command_id);
     const last_id = bot.commands[bot.commands.length - 1].id;
     const active_button = props.active_button;
+
+    const [modalActive, setModalActive] = useState(false);
     
     const onChange = (bot) => {
         props.onChangeBot(bot);
     }
+
+    
+    const FindMediaCommand = (msg_index) => {
+        let medias = [];
+        bot.mail_commands[msg_index].media.map((media) =>{
+            medias.push({
+                name: media.name,
+                type: media.type,
+                file: media.file
+            });
+        })
+        return medias;
+    }
+    const [name, setName] = useState(bot.mail_commands[message_index].name);
+    const [message, setMessage] = useState(bot.mail_commands[message_index].message);
+    const [call_date, setCallDate] = useState(bot.mail_commands[message_index].date);
+    const [media, setMedia] = useState(FindMediaCommand(message_index));
+
+
+    const [new_date_error, setNewDateError] = useState('');
 
     
     const addBlock = () => {
@@ -24,15 +52,16 @@ export function Mail(props){
         else if (active_button === "mail")
             console.log("Нельзя добавлять рассылку в это место")
         else if (active_button === "message"){
-            bot.commands[command_index].link.push(last_id + 1);
+            let guid = bot.id + Guid.newGuid()
+            bot.commands[command_index].link.push(guid);
             bot.commands.push({
-                id: last_id + 1,
+                id: guid,
                 type: "message",
                 call: [],
                 link: []
             });
             bot.message_commands.push({
-                id: last_id + 1,
+                id: guid,
                 name: "Без имени",
                 message: "",
                 media: []
@@ -63,13 +92,92 @@ export function Mail(props){
         if (props.prev_id !== null)
             //удаление ссылки на блок
             bot.commands[bot.commands.findIndex(x => x.id === props.prev_id)].link = bot.commands[bot.commands.findIndex(x => x.id === props.prev_id)].link.filter(x => x !== command_id);
-        deleteBlock(command_id);
-        onChange(bot);
+        bot = deleteBlock(command_id);
+        onChange(JSON.parse(JSON.stringify(bot)));
     }
+
+    const changeData = () => {
+        let save = true;
+
+        //Здесь должна быть проверка времени
+        if (true){
+
+            //это важно
+            setNewDateError('Ошибка');
+            save = false;
+        }
+        
+        if (save === true){
+            bot.mail_commands[message_index].name = name;
+            bot.mail_commands[message_index].message = message;
+            bot.mail_commands[message_index].date = call_date;
+            bot.mail_commands[message_index].media = CreateMediaToChange();
+            onChange(bot);
+            setNewDateError('');
+        }
+        
+    }
+    
+    const CreateMediaToChange = () => {
+        let files = [];
+        media.map((file) => {
+            let guid = command_id + Guid.newGuid();
+            files.push({
+                id: guid,
+                name: file.name,
+                type: file.type,
+                file: file.file
+            });
+        })
+        return files
+    }
+
+
+    const uploadImage = async (e) => {
+        const file = e.target.files[0];
+        const base64 = await convertBase64(file);
+        let files = media;
+        console.log("e.target")
+        console.log(e.target.files[0])
+        files.push({
+            name: e.target.files[0].name,
+            type: e.target.files[0].type,
+            file: base64
+        })
+        setMedia([...files])
+      };
+    
+      const convertBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(file);
+    
+          fileReader.onload = () => {
+            resolve(fileReader.result);
+          };
+    
+          fileReader.onerror = (error) => {
+            reject(error);
+          };
+        });
+      };
+
+      const onDeleteMedia = (file) => {
+        let medias = media;
+        medias.splice(medias.findIndex(x => x.file === file), 1)
+        setMedia([...medias])
+      }
     
     return(
         <div className='block'>
-        <div className="message-block">
+        <div className="message-block" onClick={() => { setModalActive(true);
+
+                                                        setName(bot.message_commands[message_index].name);
+                                                        setMessage(bot.message_commands[message_index].message);
+                                                        setCallDate(bot.mail_commands[message_index].date);
+                                                        setMedia(FindMediaCommand(message_index));
+                                                        setNewDateError('');
+                                                        }}>
             <div className="message-field">
                 <div>
                     <p className='text-4'>{bot.mail_commands[message_index].name}</p>
@@ -135,6 +243,67 @@ export function Mail(props){
                             </div>
                     ))}*/}
             </div>
+            {/*Окно настройки блока*/}
+            <Modal
+                active={modalActive} 
+                setActive={setModalActive}>
+                <div className='modal-head'>
+                    <p className='text-2'>Сообщение</p> 
+                    <a onClick={() => {
+                        setName(bot.message_commands[message_index].name);
+                        setMessage(bot.message_commands[message_index].message);
+                        setCallDate(bot.mail_commands[message_index].date);
+                        setMedia(FindMediaCommand(message_index));
+                        setModalActive(false);
+                    }}><img src={exitIcon} alt='Закрыть'/></a>
+                </div>
+                <hr/>
+                <form className='modal-form text-3'>
+                    <label htmlFor='name'>Название рассылки</label>
+                    <input 
+                        className='text-4' 
+                        type='text' 
+                        id='name' 
+                        placeholder='Введите название рассылки' 
+                        onChange={e => {setName(e.target.value)}}
+                        value={name}
+                        />
+                    <label htmlFor='message'>Сообщение</label>
+                    <textarea 
+                        className='message-area text-4' 
+                        type='text' 
+                        id='message' 
+                        placeholder='Введите сообщение' 
+                        onChange={e => setMessage(e.target.value)}
+                        value={message}
+                        />
+                    
+                    <label htmlFor='date'><p style={{marginTop: '0px', marginBottom: '3px'}}>Дата и время рассылки</p></label>
+                        <input 
+                            type='datetime' 
+                            id='date' 
+                            value={call_date}
+                            onChange={e => setCallDate(e.target.value)}
+                        />
+                    <div className='error-message text-5'><p style={{color: "red"}}>{new_date_error}</p></div>
+
+                    <label htmlFor='file'><p style={{marginTop: '0px', marginBottom: '3px'}}>Файлы</p></label>
+                    {console.log(media)}
+                    
+                    
+                    {modalActive && media.map((obj) => (
+                        <div key={obj.name}>
+                            <FileList onDelete={onDeleteMedia} file={obj.file} name={obj.name} type={obj.type}/>
+                        </div>
+                    ))}
+                    <input 
+                        type='file' 
+                        accept='image/*, video/*, application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+                        onChange={e => uploadImage(e)}>
+                    </input>
+                    <button onClick={() => changeData()}>Сохранить</button>
+                </form>
+            </Modal>
         </div>
     );
 }
